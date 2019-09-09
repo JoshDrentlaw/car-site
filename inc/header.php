7383be8b1414104a5adbd64ecb3464e9591cdc79
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-09-08 19:26:28 -0700

    Changed includes to requires.

--- inc/header.php
+++ inc/header.php
@@ -10,5 +10,5 @@
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="contact.php">Contact</a></li>
     </ul>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php include 'inc/header.php'; ?>
+<?php require 'inc/header.php'; ?>
     <h1>Home</h1>
-<?php include 'inc/footer.php'; ?>
+<?php require 'inc/footer.php'; ?>
