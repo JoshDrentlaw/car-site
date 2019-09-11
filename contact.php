c8b4e20d5a8ec97a94511bf6a348793e9913ebed
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-09-11 09:24:37 -0700

    Added succcess page.

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
 ?>
 <?php require 'inc/header.php'; ?>
     <h1>Contact</h1>
-    <form method="POST" action="contact.php">
+    <form method="POST" action="success.php">
         <div>
             <label for="name">Name</label><br>
             <input type="text" name="name">
@@ -15,5 +15,6 @@
             <label for="email">Email</label><br>
             <input type="email" name="email">
         </div>
+        <button type="submit">submit</button>
     </form>
 <?php require 'inc/footer.php'; ?>
---
+++ success.php
@@ -0,0 +1,3 @@
+<?php require 'inc/header.php' ?>
+    <h1>Thank you $name for your interest!</h1>
+<?php require 'inc/footer.php' ?>
