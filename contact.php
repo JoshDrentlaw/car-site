3018ed9356bb7470b4de9a5e36a6212d91162b4c
Josh Drentlaw <joshdrentlaw@gmail.com>
2019-09-11 00:19:02 -0700

    Added form to contact.

--- contact.php
+++ contact.php
@@ -1,3 +1,13 @@
 <?php require 'inc/header.php'; ?>
     <h1>Contact</h1>
+    <form method="GET" action="contact.php">
+        <div>
+            <label for="name">Name</label><br>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="email">Email</label><br>
+            <input type="email" name="email">
+        </div>
+    </form>
 <?php require 'inc/footer.php'; ?>
